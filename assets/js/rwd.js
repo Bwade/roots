7ac57a051eda3df14f2309d51cafc96146bb20c1
Bob Wade <bwade231@gmail.com>
2014-02-23 20:13:09 -0600

    Style Edits

--- assets/js/rwd.js
+++ assets/js/rwd.js
@@ -8,34 +8,34 @@ var headroom  = new Headroom(myElement);
 // initialise
 headroom.init();
 
-/* Tracking clicks like a fucking champ 
+/* Tracking clicks like a fucking champ */
 
 $('#button').on('click', function() {
-	ga('send', 'event', 'button', 'click', 'nav-buttons');
+ga('send', 'event', 'button', 'click', 'nav-buttons');
 });
-*/
+
 
 var $win = $(window),$home_main = $('.page-home #main');
-			
+
 //Viewport Checking and shit
-$(window).resize(function() {
-	var $win = $(this), win_ratio;
-		//my_class;
+//$(window).resize(function() {
+//var $win = $(this), win_ratio;
+//	//my_class;
 
-	if ($home_main.length > 0) {
-		// Figure out if browser is wider or taller than video
-		win_ratio = $home_main.width()/$home_main.height();
-		//my_class = (win_ratio >= vid_ratio)?'wide':'tall';
+//	if ($home_main.length > 0) {
+//	// Figure out if browser is wider or taller than video
+//	win_ratio = $home_main.width()/$home_main.height();
+//	//my_class = (win_ratio >= vid_ratio)?'wide':'tall';
 
-		// Apply wide/tall as a class to the #page container
-		$('#page').removeClass('wide').removeClass('tall').addClass(my_class);
-		$('#main .pane').height( $(this).height() );
-	}
-	
-});
+//	// Apply wide/tall as a class to the #page container
+//	$('#page').removeClass('wide').removeClass('tall').addClass(my_class);
+//		$('#main .pane').height( $(this).height() );
+//}
+
+// });
 
 //vCard Tabs
 $('#myTab a').click(function (e) {
-	e.preventDefault();
-	$(this).tab('show');
+e.preventDefault();
+$(this).tab('show');
 });
